ce555905408478a899d066624daa716ccc3e68b1
Murad <muradmustafayev03@gmail.com>
2024-02-20 16:39:08 +0400

    created Project class

---
+++ Project.py
@@ -0,0 +1,21 @@
+import os
+
+from Package import Package
+
+
+class Project:
+    def __init__(self, path, name):
+        self.name = name
+        self.path = path
+
+        self.packages = []
+        self.classes = []
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    def parse_packages(self):
+        pass
