5fd387a459d24f23d849d8d581b27f86a8d1e7d1
Murad <muradmustafayev03@gmail.com>
2024-02-20 18:27:32 +0400

    parse packages in a project

--- src/Package.py
+++ src/Package.py
@@ -1,9 +1,7 @@
 class Package:
     def __init__(self, name):
         self.name = name
-        self.classes = []
         self.paths = []
-        self.subpackages = []
 
     def __str__(self):
         return self.name
@@ -13,21 +11,6 @@ class Package:
 
     def __add__(self, other):
         package = Package(self.name + '+' + other.name)
-        package.classes = self.classes + other.classes
         package.paths = self.paths + other.paths
 
-        if other in self.subpackages:
-            self.subpackages.remove(other)
-        if self in other.subpackages:
-            other.subpackages.remove(self)
-
-        package.subpackages = self.subpackages + other.subpackages
-
         return package
-
-    def add_class(self, class_name: str, path: str):
-        self.classes.append(class_name)
-        self.paths.append(path)
-
-    def add_subpackage(self, package):
-        self.subpackages.append(package)
--- src/Project.py
+++ src/Project.py
@@ -1,3 +1,9 @@
+import os
+
+from src.Package import Package
+from src.parsers import parse_package
+
+
 class Project:
     def __init__(self, path, name):
         self.name = name
@@ -12,5 +18,35 @@ class Project:
     def __repr__(self):
         return self.name
 
+    def traverse_apply(self, func: callable):
+        """
+        Traverse the directory and apply the function to each java file
+        Note: the function should take a single argument - path: str
+        """
+        for root, dirs, files in os.walk(self.path):
+            for file in files:
+                if not file.endswith(".java"):
+                    continue
+
+                path = os.path.join(root, file)
+                func(path)
+
     def parse_packages(self):
-        pass
+        def parse_package_from_file(path: str):
+            with open(path, "r") as f:
+                code = f.read()
+            package_name = parse_package(code)
+
+            if package_name is None:
+                return
+
+            candidate = [p for p in self.packages if p.name == package_name]
+
+            if not candidate:
+                package = Package(package_name)
+                package.paths.append(str(path))
+                self.packages.append(package)
+            else:
+                candidate[0].paths.append(str(path))
+
+        self.traverse_apply(parse_package_from_file)
