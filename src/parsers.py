64d4abd60aa5f975ba0ccbc8afc5bbed82e4a90d
Murad <muradmustafayev03@gmail.com>
2024-02-20 17:02:01 +0400

    added functions to parsers.py

--- src/parsers.py
+++ src/parsers.py
@@ -1,6 +1,10 @@
 import re
 
 
+def remove_empty_lines(code: str) -> str:
+    return '\n'.join([line for line in code.split('\n') if line.strip()])
+
+
 def remove_singe_line_comments(code: str) -> str:
     lines = [line.split('//')[0] for line in code.split('\n')]
     lines = [line for line in lines if line.strip()]
@@ -9,3 +13,9 @@ def remove_singe_line_comments(code: str) -> str:
 
 def remove_multi_line_comments(code: str) -> str:
     return re.sub(r'/\*.*?\*/', '', code, flags=re.DOTALL)
+
+
+def remove_comments(code: str) -> str:
+    code = remove_singe_line_comments(code)
+    code = remove_multi_line_comments(code)
+    return code
